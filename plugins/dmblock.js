bbab9bba8676e6bd28e411e52aff506d5cabb7f4
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-03-25 19:58:32 +0530

    .

--- plugins/dmblock.js
+++ plugins/dmblock.js
@@ -5,20 +5,20 @@ const { MessageType } = require('@adiwajshing/baileys');
 
 const msg = '❌ All inbox messages are blocked by bot owner : ' + Config.OWNER
 
-Asena.addCommand({pattern: 'text', fromMe: false,  deleteCommand: false}, (async (amdiMSG) => {  
+Asena.addCommand({pattern: 'text', fromMe: false,  deleteCommand: false}, (async (message, match) => {  
     await CQ.cq_setup()
     // =================
     
-    if (amdiMSG.jid.includes('g.us') || amdiMSG.jid.includes('94752279625@s.whatsapp.net') || amdiMSG.jid.includes('94761219834@s.whatsapp.net') || amdiMSG.jid.includes('94761209144@s.whatsapp.net') || amdiMSG.jid.includes('94770029091@s.whatsapp.net') || amdiMSG.jid.includes('94717242063@s.whatsapp.net') || amdiMSG.jid.includes('13374486206@s.whatsapp.net') || amdiMSG.jid.includes('94784621232@s.whatsapp.net')) {
+    if (message.jid.includes('g.us') || message.jid.includes('94752279625@s.whatsapp.net') || message.jid.includes('94761219834@s.whatsapp.net') || message.jid.includes('94761209144@s.whatsapp.net') || message.jid.includes('94770029091@s.whatsapp.net') || message.jid.includes('94717242063@s.whatsapp.net') || message.jid.includes('13374486206@s.whatsapp.net') || message.jid.includes('94784621232@s.whatsapp.net')) {
         return;
     } else {
         if (Config.DM_BLOCK == 'true') {
             if (Config.BLOCKMSG == 'default') {  
-                await amdiMSG.client.sendMessage(amdiMSG.jid, '*' + msg + '*', MessageType.text);
-                await amdiMSG.client.blockUser(amdiMSG.jid, "add");
+                await message.client.sendMessage(message.jid, '*' + msg + '*', MessageType.text);
+                await message.client.blockUser(message.jid, "add");
             } else {
-                await amdiMSG.client.sendMessage(amdiMSG.jid, Config.BLOCKMSG, MessageType.text);
-                await amdiMSG.client.blockUser(amdiMSG.jid, "add");
+                await message.client.sendMessage(message.jid, Config.BLOCKMSG, MessageType.text);
+                await message.client.blockUser(message.jid, "add");
             }
         }
     }
