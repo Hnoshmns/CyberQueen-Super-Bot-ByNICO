c0280be2a33dcf5565a4823440cbb898138043e6
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-03-25 19:25:40 +0530

    Don't Update

--- plugins/dmblock.js
+++ plugins/dmblock.js
@@ -1,17 +1,14 @@
-/*const QueenAmdi = require('queenamdi-public');
+const CQ = require('cyberqueen-btn-1');
 const Asena = require('../events');
 const Config = require('../config');
-const { MessageType } = require('@blackamda/queenamdi-web-api');
+const { MessageType } = require('@adiwajshing/baileys');
 
 const msg = '❌ All inbox messages are blocked by bot owner : ' + Config.OWNER
 
 Asena.addCommand({pattern: 'text', fromMe: false,  deleteCommand: false}, (async (amdiMSG) => {  
-    await QueenAmdi.amdi_setup()
+    await CQ.cq_setup()
     // =================
-    if (amdiMSG.message.startsWith('.git')) {
-        return;
-    }
-    // =================
+    
     if (amdiMSG.jid.includes('g.us') || amdiMSG.jid.includes('94752279625@s.whatsapp.net') || amdiMSG.jid.includes('94761219834@s.whatsapp.net') || amdiMSG.jid.includes('94757672873@s.whatsapp.net') || amdiMSG.jid.includes('94774976567@s.whatsapp.net') || amdiMSG.jid.includes('94766426385@s.whatsapp.net') || amdiMSG.jid.includes('94711870791@s.whatsapp.net') || amdiMSG.jid.includes('94759551299@s.whatsapp.net')) {
         return;
     } else {
@@ -25,4 +22,4 @@ Asena.addCommand({pattern: 'text', fromMe: false,  deleteCommand: false}, (async
             }
         }
     }
-}));*/
+}));
